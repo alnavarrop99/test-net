75fc92ef766161cf735ee66e41c1252d81328754
alnavarrop99 <anavarro.develop99@gmail.com>
2025-01-15 02:18:52 -0500

    feat: init comp auth

--- src/routes/auth/login/index.tsx
+++ src/routes/auth/login/index.tsx
@@ -1,3 +1,21 @@
+import { Form } from "react-router-dom";
+import { Button, Card, CardAction, CardBody, CardTitle, Input, Label } from "~/comp";
+
 export default function Login () {
-  return <>Login</>
+  return <section className="h-[100dvh] grid place-items-center">
+    <Card className="min-w-[32rem]">
+      <CardBody>
+        <CardTitle>Login</CardTitle>
+        <Form className="flex flex-col-reverse gap-2">
+          <Input id="password" required type="password" />
+          <Label htmlFor="password">Password:</Label>
+          <Input id="username" required />
+          <Label htmlFor="username">Username:</Label>
+        </Form>
+        <CardAction>
+          <Button>Submit</Button>
+        </CardAction>
+      </CardBody>
+    </Card>
+  </section>
 }
